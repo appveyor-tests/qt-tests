a412766de88d4776fae06530824face9a124ed4c
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-04-12 08:09:55 -0700

    bump to 6.4.3

--- test.ps1
+++ test.ps1
@@ -138,7 +138,7 @@ if ((test-path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community") 
             "msvc2019_64"
             "msvc2019_arm64"
         )
-        @("6.4", "6.4.2") = @(
+        @("6.4", "6.4.3") = @(
             "mingw_64"
             "msvc2019_64"
             "msvc2019_arm64"
