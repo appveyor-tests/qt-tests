419e548634bc533f9ec87c6767cd9c3a8ce89c28
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-09-05 09:51:59 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -138,7 +138,7 @@ if ((test-path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community") 
             "msvc2019_64"
             "msvc2019_arm64"
         ) 
-        @("6.5", "6.5.1") = @(
+        @("6.5", "6.5.2") = @(
             "mingw_64"
             "msvc2019_64"
             "msvc2019_arm64"
